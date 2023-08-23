0d30b4a5a8d7f8a32fdb26848e32a38174ee497d
razan0010 <132309368+razan0010@users.noreply.github.com>
2023-08-23 14:48:10 +0300

    Add files via upload

---
+++ java/com/example/personalshoppersystem/PersonalShopperSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalshoppersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalShopperSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalShopperSystemApplication.class, args);
+    }
+
+}
